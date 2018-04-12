4b221ef378ba08537cb6b65ec751abc3b0fbee2f
Nyimbi Odero <nyimbi@gmail.com>
2018-04-12 11:16:42 +0300

    Update generate_ssl_key.sh

--- generate_ssl_key.sh
+++ generate_ssl_key.sh
@@ -5,10 +5,10 @@ domain=$1
 commonname=$domain
  
 #Change to your company details
-country=NO
-state=Rogaland
-locality=Sola
-organization=example.org
+country=KE
+state=Nairobi
+locality=Nairobi
+organization=ajira.world
 
 openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 3600 \
 	-nodes \
